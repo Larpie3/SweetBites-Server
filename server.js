388deaea340e7ac4f38929c9908650da12d37ab7
Ralph Castañares <ralphcstnrs3@gmail.com>
2025-10-30 00:53:32 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-import express from 'express';
-import cors from 'cors';
-import fetch from 'node-fetch';
+import express from "express";
+import cors from "cors";
+import fetch from "node-fetch";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 // Enable CORS for all origins
 app.use(cors());
